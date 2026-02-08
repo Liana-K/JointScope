6919a264f7ae14156fd135a42d5a72366327b851
Khwaish_Kapoor <130338279+KKapoor0001@users.noreply.github.com>
2026-02-08 03:40:15 +0000

    deleted global.css

--- frontend/my-react-app/src/main.jsx
+++ frontend/my-react-app/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "./styles/global.css";
 import "./styles/layout.css";
 import "./styles/buttons.css";
 import "./styles/login.css";
