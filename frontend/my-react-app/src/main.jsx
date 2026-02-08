1c636fea9b1d9d65360095b5819bf0e28c9292e3
Khwaish_Kapoor <130338279+KKapoor0001@users.noreply.github.com>
2026-02-08 04:40:20 +0000

    shree venkateshwara

--- frontend/my-react-app/src/main.jsx
+++ frontend/my-react-app/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./styles/charts.css";
 import "./styles/layout.css";
 import "./styles/buttons.css";
-import "./styles/login.css";
+import "./styles/dashboard.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
