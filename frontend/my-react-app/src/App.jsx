54febfcea3b6a54c01bb93bad8f3675f26014f63
Khwaish_Kapoor <130338279+KKapoor0001@users.noreply.github.com>
2026-02-08 04:35:21 +0000

    jai baba bhole

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,33 +1,19 @@
 import { useState } from "react";
 
-import Splash from "./pages/Splash";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Record from "./pages/Record";
-import History from "./pages/History";
+import Splash from "./Pages/Splash";
+import Login from "./Pages/Login";
+import Dashboard from "./Pages/Dashboard";
+import Record from "./Pages/Record";
+import History from "./Pages/History";
 
 export default function App() {
   const [page, setPage] = useState("splash");
 
-  if (page === "splash") {
-    return <Splash goTo={setPage} />;
-  }
-
-  if (page === "login") {
-    return <Login goTo={setPage} />;
-  }
-
-  if (page === "dashboard") {
-    return <Dashboard goTo={setPage} />;
-  }
-
-  if (page === "record") {
-    return <Record goTo={setPage} />;
-  }
-
-  if (page === "history") {
-    return <History goTo={setPage} />;
-  }
+  if (page === "splash") return <Splash goTo={setPage} />;
+  if (page === "login") return <Login goTo={setPage} />;
+  if (page === "dashboard") return <Dashboard goTo={setPage} />;
+  if (page === "record") return <Record goTo={setPage} />;
+  if (page === "history") return <History goTo={setPage} />;
 
   return null;
 }
