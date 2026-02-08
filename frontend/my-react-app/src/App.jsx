322cde9f9fcef16cbebf6f336204a085f9d78eed
Khwaish_Kapoor <130338279+KKapoor0001@users.noreply.github.com>
2026-02-08 04:07:53 +0000

    om shanti om

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Splash from "./pages/Splash.jsx";
-import Login from "./pages/Login.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-import Record from "./pages/Record.jsx";
-import History from "./pages/History.jsx";
+import Splash from "./pages/Splash";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Record from "./pages/Record";
+import History from "./pages/History";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Splash" element={<Splash />} />
+        <Route path="/" element={<Splash />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/record" element={<Record />} />
