14772974e79d758bd54b2ec8a59326fe4c6e7863
Khwaish_Kapoor <130338279+KKapoor0001@users.noreply.github.com>
2026-02-08 03:50:20 +0000

    edited changes

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,6 +1,21 @@
-// src/App.jsx
-import RecoveryTest from "./RecoveryTest";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Splash from "./pages/Splash";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Record from "./pages/Record";
+import History from "./pages/History";
 
 export default function App() {
-  return <RecoveryTest />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/record" element={<Record />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- frontend/my-react-app/src/main.jsx
+++ frontend/my-react-app/src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import "./styles/charts.css";
 import "./styles/layout.css";
 import "./styles/buttons.css";
-import "./styles/login.css";
+import "./styles/dashboard.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
